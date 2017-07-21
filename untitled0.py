03a1d2b0ae2ba060983de402aa21d4c7ebb6d972
JTTran <tranjt80@gmail.com>
2017-07-21 10:51:53 +0300

    Add problem set 5

--- untitled0.py
+++ untitled0.py
@@ -1,25 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jun 20 19:43:37 2017
+Created on Tue Jul 18 23:16:31 2017
 
 @author: Zun
 """
 
-def biggest(aDict):
-    '''
-    aDict: A dictionary, where all the values are lists.
+class Person:
 
-    returns: The key with the largest number of values associated with it
-    '''
-    # Your Code Here
-    largestAnimalCount = len(max(aDict.values()))
-    print(largestAnimalCount)
-    biggest = []
-    
-    for animals in aDict:
-        if len(aDict[animals]) == largestAnimalCount:
-            biggest.append(animals)
-        return biggest
-    
-print(biggest({'b': [1, 7, 5, 4, 3, 18, 10, 0], 'a': []}))
+    def __init__(self, first, last):
+        self.firstname = first
+        self.lastname = last
 
+    def Name(self):
+        return self.firstname + " " + self.lastname
+    
+    def get_staffnumber(self):
+        return self.staffnumber
+
+class Employee(Person):
+
+    def __init__(self, first, last, staffnum):
+        Person.__init__(self)
+        self.staffnumber = staffnum
+
+    def GetEmployee(self):
+        return self.Name() + ", " +  self.staffnumber
+    
+
+
+x = Person("Marge", "Simpson")
+y = Employee("Homer", "Simpson", "1007")
+
+print(x.Name())
+#print(y.GetEmployee())
+print(y.get_staffnumber())
